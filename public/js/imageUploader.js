92267dfd430af27e1f9c4c2fdf99b52a1f35c019
Arshdeep Singh Padda <aspadda@icloud.com>
2017-09-06 20:56:58 -0700

    Page now shows current image and allows you to upload and preview your own

--- public/js/imageUploader.js
+++ public/js/imageUploader.js
@@ -1,4 +1,13 @@
 $('#imageInput').on('change', function() {
+
     var file = document.getElementById('imageInput').files[0];
+
     $('#fileName').text(file.name);
+
+    reader = new FileReader();
+    reader.onload = function (e) {
+        $('.card-img-top').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(file);
+
 });
